ede53c1011d6e7e0a377b31832cf54fa912865e0
retrilzzy <retrilzzy@gmail.com>
2024-10-30 23:30:05 +0500

    обновлен скрипт для изменения обоев

--- Configs/hypr/scripts/wallpaper.sh
+++ Configs/hypr/scripts/wallpaper.sh
@@ -1,8 +1,32 @@
 #!/bin/bash
 
 WALLPAPER_DIR="$HOME/Pictures/Wallpapers"
-
 INDEX_FILE="$HOME/.current_wallpaper_index"
+
+while [[ $# -gt 0 ]]; do
+  case $1 in
+    -d|--directory)
+      WALLPAPER_DIR="$2"
+      shift 2
+      ;;
+    -n|--next)
+      ACTION="next"
+      shift
+      ;;
+    -p|--prev)
+      ACTION="prev"
+      shift
+      ;;
+    -c|--clear)
+      ACTION="clear"
+      shift
+      ;;
+    *)
+      echo "Неправильный флаг: $1"
+      exit 1
+      ;;
+  esac
+done
 
 if [ ! -f "$INDEX_FILE" ]; then
   echo 0 > "$INDEX_FILE"
@@ -13,10 +37,14 @@ WALLPAPER_COUNT=${#WALLPAPERS[@]}
 
 CURRENT_INDEX=$(<"$INDEX_FILE")
 
-if [[ "$1" == "-next" ]]; then
+if [[ "$ACTION" == "next" ]]; then
   ((CURRENT_INDEX=(CURRENT_INDEX + 1) % WALLPAPER_COUNT))
-elif [[ "$1" == "-prev" ]]; then
+elif [[ "$ACTION" == "prev" ]]; then
   ((CURRENT_INDEX=(CURRENT_INDEX - 1 + WALLPAPER_COUNT) % WALLPAPER_COUNT))
+elif [[ "$ACTION" == "clear" ]]; then
+  hyprctl hyprpaper unload all
+  notify-send "Все обои выгружены."
+  exit 0
 else
   exit 0
 fi
