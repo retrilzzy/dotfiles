231102154f915a3e72543d8f589037a74f80bfd5
retrilzzy <retrilzzy@gmail.com>
2025-02-27 22:05:33 +0500

    Обновлен скрипт для подстановки текущих обоев в hyprlock

--- Configs/.config/wlogout/current_wall.sh
+++ Configs/.config/wlogout/current_wall.sh
@@ -1,30 +1,8 @@
 #!/bin/bash
 
-WALLPAPER_DIR="$HOME/Pictures/Wallpapers"
-INDEX_FILE="$HOME/.current_wallpaper_index"
 TARGET_DIR="$HOME/Pictures"
 
-while [[ $# -gt 0 ]]; do
-  case $1 in
-    -d|--directory)
-      WALLPAPER_DIR="$2"
-      shift 2
-      ;;
-    *)
-      echo "Неправильный флаг: $1"
-      exit 1
-      ;;
-  esac
-done
-
-if [ ! -f "$INDEX_FILE" ] || [ ! -s "$INDEX_FILE" ]; then
-  echo 1 > "$INDEX_FILE"
-fi
-
-WALLPAPERS=("$WALLPAPER_DIR"/*)
-CURRENT_INDEX=$(<"$INDEX_FILE")
-
-SELECTED_WALLPAPER="${WALLPAPERS[$CURRENT_INDEX]}"
+SELECTED_WALLPAPER=$(waypaper --list | sed -n 's/.*"wallpaper":\s*"\(.*\)".*/\1/p')
 
 
 cp "$SELECTED_WALLPAPER" "$TARGET_DIR/wlogoutbg"
