73566dfc935a562b8a169d46f8aa2d385441cfe1
retrilzzy <retrilzzy@gmail.com>
2025-04-02 22:30:01 +0500

    Рефактор скрипта установки пакетов для риса

--- Scripts/install_packages.sh
+++ Scripts/install_packages.sh
@@ -4,61 +4,81 @@ set -e
 cd ~
 
 install() {
-    read -r -p "Установить $1? (y/N): " choice
+    read -r -p "❔ Установить $1? (y/N): " choice
     case "$choice" in 
         y|Y|yes|Yes|YES ) eval "$2" ;;
-        * ) echo "Пропущено: $1" ;;
+        * ) echo "🛫 Пропущено: $1" ;;
     esac
 }
 
 
-echo "Обновление репозиториев и системы..."
+echo "📥️  Обновление репозиториев и системы..."
 sudo pacman -Syu
 
-echo "⚠️ Учтите, что отсутствие некоторых пакетов приведет к некорректной работе системы!"
 
-install "Жизненно необходимые пакеты (git, curl)" \
+echo "⚠️  Учтите, что отсутствие некоторых пакетов приведет к некорректной работе системы!"
+
+
+install "Git, Curl (жизненно необходимые пакеты)" \
     "sudo pacman -S --noconfirm git curl"
+
 
 install "Yay (AUR помощник) [Без Yay вы не сможете установить часть пакетов!]" \
     "sudo pacman -S --needed base-devel && git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si"
 
 
-install "Тема курсора (rose-pine)" "yay -S --noconfirm rose-pine-cursor rose-pine-hyprcursor"
-
-install "Xdg-utils, Xdg-desktop-portal (утилиты для работы с XDG)" \
-    "sudo pacman -S --noconfirm xdg-utils xdg-desktop-portal-hyprland xdg-desktop-portal-gtk xdg-desktop-portal-wlr xdg-desktop-portal"
-
-
 install "Network Manager (сетевые инструменты)" \
     "sudo pacman -S --noconfirm network-manager network-manager-applet"
 
-install "Bluez, Blueman (Bluetooth инструменты)" \
+
+install "Bluez, Blueman (bluetooth инструменты)" \
     "sudo pacman -S --noconfirm bluez bluez-tools blueman"
+
+
+install "Xdg-utils, Xdg-desktop-portal (интеграция приложений с рабочим столом)" \
+    "sudo pacman -S --noconfirm xdg-utils xdg-desktop-portal-hyprland xdg-desktop-portal-gtk xdg-desktop-portal-wlr xdg-desktop-portal"
 
 
 install "Brightnessctl (управления яркостью)" \
     "sudo pacman -S --noconfirm brightnessctl"
 
 
+install "playerctl (управление медиа плеером)" \
+    "sudo pacman -S --noconfirm playerctl"
+
+
+install "Rose Pine (тема курсора)" \
+    "yay -S --noconfirm rose-pine-cursor rose-pine-hyprcursor"
+
+
+install "Papirus (иконки)" \
+    "sudo pacman -S --noconfirm papirus-icon-theme"
+
+
 install "Grim, Hyprshot (cкриншоты)" \
     "sudo pacman -S --noconfirm grim && yay -S --noconfirm hyprshot"
 
 
-install "Waybar (панель)" "sudo pacman -S --noconfirm waybar"
+install "Wlogout (меню выключения)" \
+    "yay -S --noconfirm wlogout"
 
 
-install "Rofi (меню запуска)" "sudo pacman -S --noconfirm rofi"
-
-install "Cliphist (буфер обмена)" "sudo pacman -S --noconfirm wl-clipboard cliphist"
-
-
-install "Wlogout (меню выключения)" "yay -S --noconfirm wlogout"
+install "Waybar (wayland бар)" \
+    "sudo pacman -S --noconfirm waybar"
 
 
-install "Kitty (терминал)" "sudo pacman -S --noconfirm kitty"
+install "Rofi (меню запуска + меню буфера обмена)" \
+    "sudo pacman -S --noconfirm rofi"
 
-install "Zsh + Oh My Zsh (оболочка)" \
+
+install "Cliphist (буфер обмена)" \
+    "sudo pacman -S --noconfirm wl-clipboard cliphist"
+
+
+install "Kitty (эмулятор терминала)" \
+    "sudo pacman -S --noconfirm kitty"
+
+install "Zsh + Oh My Zsh (оболочка + менеджер плагинов)" \
     "sudo pacman -S --noconfirm zsh \
         && sh -c \"$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)\""
 
@@ -70,26 +90,26 @@ install "Плагины для Zsh" \
     git clone https://github.com/zsh-users/zsh-autosuggestions \${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions"
 
 
-install "Fastfetch (информация о системе)" "sudo pacman -S --noconfirm fastfetch"
+install "Nwg-look (настройка GTK)" "sudo pacman -S --noconfirm nwg-look"
 
 
-install "Nwg-look (настройка GTK3)" "sudo pacman -S --noconfirm nwg-look"
+install "Swaync (уведомления похожие на GNOME)" "sudo pacman -S --noconfirm swaync"
 
 
-install "Swaync (уведомления)" "sudo pacman -S --noconfirm swaync"
+install "Waypaper (интерфейс для управления обоями)" "yay -S --noconfirm waypaper"
 
+install "Swww (бэкенд для статичных обоев)" "sudo pacman -S --noconfirm swww"
 
-install "Waypaper (обои)" "yay -S --noconfirm waypaper"
-
-install "Swww (статичные обои)" "sudo pacman -S --noconfirm swww"
-
-install "Mpvpaper (видео обои)" "sudo pacman -S --noconfirm mpvpaper"
+install "Mpvpaper (бэкенд для видео обоев)" "sudo pacman -S --noconfirm mpvpaper"
 
 
 install "Emote (выбор эмодзи)" "yay -S --noconfirm emote"
 
 
-install "Flameshot (утилита для скриншотов)" "sudo pacman -S --noconfirm flameshot"
+install "Flameshot (мощная утилита для скриншотов)" "sudo pacman -S --noconfirm flameshot"
+
+
+install "Fastfetch (похвастаться линуксом)" "sudo pacman -S --noconfirm fastfetch"
 
 
 echo "✅ Установка завершена!"
