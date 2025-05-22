abcaeffff20b20412eead1524fca53f1fd36abeb
retrilzzy <retrilzzy@gmail.com>
2025-05-22 22:57:18 +0500

    Изменения конфигурации и пути до фона hyprlock

--- Configs/.config/wlogout/current_wall.sh
+++ Configs/.config/wlogout/current_wall.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-TARGET_DIR="$HOME/Pictures"
+TARGET_DIR="$HOME/.cache"
 
 SELECTED_WALLPAPER=$(waypaper --list | waypaper --list | jq -r '.[0].wallpaper')
 
 
-cp "$SELECTED_WALLPAPER" "$TARGET_DIR/wlogoutbg"
+cp "$SELECTED_WALLPAPER" "$TARGET_DIR/wlogout_background"
