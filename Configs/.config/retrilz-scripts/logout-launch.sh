bc021553e0440b7896f70a67a9615a62ffc92b4b
retrilzzy <retrilzzy@gmail.com>
2025-11-09 23:43:49 +0500

    Добавлена поддержка палитры Matugen для Wlogout + рефактор скрипта его запуска

--- Configs/.config/retrilz-scripts/logout-launch.sh
+++ Configs/.config/retrilz-scripts/logout-launch.sh
@@ -10,42 +10,19 @@ fi
 confDir="${XDG_CONFIG_HOME:-$HOME/.config}"
 
 # Style selection
-[ -z "${1}" ] || wlogoutStyle="${1}"
-wLayout="${confDir}/wlogout/layout_${wlogoutStyle}"
-wlTmplt="${confDir}/wlogout/style_${wlogoutStyle}.css"
-
-if [ ! -f "${wLayout}" ] || [ ! -f "${wlTmplt}" ]; then
-    echo "ERROR: Config ${wlogoutStyle} not found..."
-    wlogoutStyle=1
-    wLayout="${confDir}/wlogout/layout_${wlogoutStyle}"
-    wlTmplt="${confDir}/wlogout/style_${wlogoutStyle}.css"
-fi
+wLayout="${confDir}/wlogout/layout"
+wlTmplt="${confDir}/wlogout/style.css"
 
 # Detect monitor resolution
-x_mon=$(hyprctl -j monitors | jq '.[] | select(.focused==true) | .width')
 y_mon=$(hyprctl -j monitors | jq '.[] | select(.focused==true) | .height')
 hypr_scale=$(hyprctl -j monitors | jq '.[] | select(.focused == true) | .scale' | sed 's/\.//')
 
-# Scale config layout and style
-case "${wlogoutStyle}" in
-1)
-    wlColms=6
-    export mgn=$((y_mon * 38 / hypr_scale))
-    export hvr=$((y_mon * 33 / hypr_scale))
-    ;;
-2)
-    wlColms=2
-    export x_mgn=$((x_mon * 38 / hypr_scale))
-    export y_mgn=$((y_mon * 28 / hypr_scale))
-    export x_hvr=$((x_mon * 35 / hypr_scale))
-    export y_hvr=$((y_mon * 23 / hypr_scale))
-    ;;
-esac
+wlColms=6
+export mgn=$((y_mon * 38 / hypr_scale))
+export hvr=$((y_mon * 33 / hypr_scale))
 
 # Scale font size
 export fntSize=$((y_mon * 2 / 100))
-
-export BtnCol="white" # Default button color
 
 # Evaluate hypr border radius
 hypr_border="$(hyprctl -j getoption decoration:rounding | jq '.int')"
